ba5f368934fc5cbc5bca0ed3f05501555b4e2c6b
Itamar Carvalho <itamarc@gmail.com>
2024-02-07 01:47:35 -0300

    Including an open lock image for free button Solving warnings about possible lambdas

--- app/src/main/java/io/github/itamarc/magnifier/MainActivity.java
+++ app/src/main/java/io/github/itamarc/magnifier/MainActivity.java
@@ -4,41 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     private boolean flashlightOn = false;
+    private boolean imageFrozen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton fabFreeze = findViewById(R.id.btnFreeze);
+        fabFreeze.setOnClickListener(v -> {
+            // android:src="@drawable/baseline_flashlight_off_24" />
+            if (imageFrozen) {
+                fabFreeze.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.baseline_lock_open_24, getTheme()));
+                // TODO unfreeze image
+                imageFrozen = false;
+            } else {
+                fabFreeze.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.baseline_lock_24, getTheme()));
+                // TODO freeze image
+                imageFrozen = true;
+            }
+        });
+
         FloatingActionButton fabFlashlight = findViewById(R.id.btnFlashlight);
-        fabFlashlight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // android:src="@drawable/baseline_flashlight_off_24" />
-                if (flashlightOn) {
-                    fabFlashlight.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.baseline_flashlight_off_24, getTheme()));
-                    // TODO turn flashlight off
-                    flashlightOn = false;
-                } else {
-                    fabFlashlight.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.baseline_flashlight_on_24, getTheme()));
-                    // TODO turn flashlight on
-                    flashlightOn = true;
-                }
+        fabFlashlight.setOnClickListener(v -> {
+            // android:src="@drawable/baseline_flashlight_off_24" />
+            if (flashlightOn) {
+                fabFlashlight.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.baseline_flashlight_off_24, getTheme()));
+                // TODO turn flashlight off
+                flashlightOn = false;
+            } else {
+                fabFlashlight.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.baseline_flashlight_on_24, getTheme()));
+                // TODO turn flashlight on
+                flashlightOn = true;
             }
         });
 
         FloatingActionButton fabExit = findViewById(R.id.btnExit);
-        fabExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.exit(0);
-            }
-        });
+        fabExit.setOnClickListener(v -> System.exit(0));
     }
 }
